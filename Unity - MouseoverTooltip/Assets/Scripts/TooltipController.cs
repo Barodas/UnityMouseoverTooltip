3ad71d00f9c5535685e2ee9c72a9d474fe554e65
Luke Savage <lsava22@hotmail.com>
2017-05-22 17:07:53 +1000

    Created Tooltip object and Script to follow mouse
    -Added UI Canvas and Panels

---
+++ Unity - MouseoverTooltip/Assets/Scripts/TooltipController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipController : MonoBehaviour
+{
+    public Text TooltipText;
+
+    private RectTransform _rect;
+    private bool _isHovering;
+    private Vector3 _offset;
+
+	void Start ()
+    {
+        _rect = GetComponent<RectTransform>();
+	}
+	
+	void Update ()
+    {
+        _offset = new Vector3(_rect.rect.width * 0.5f, -(_rect.rect.height * 0.5f), 0);
+        transform.position = Input.mousePosition + _offset;
+	}
+}
