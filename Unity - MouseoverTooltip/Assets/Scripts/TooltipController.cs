27e09b7d24561afdef211eafa0a492692282fa89
Luke Savage <lsava22@hotmail.com>
2017-05-29 15:51:39 +1000

    Added handling for tooltips going off screen
    -Added Padding value from mouse

--- Unity - MouseoverTooltip/Assets/Scripts/TooltipController.cs
+++ Unity - MouseoverTooltip/Assets/Scripts/TooltipController.cs
@@ -7,23 +7,57 @@ public class TooltipController : MonoBehaviour
 
     public GameObject TooltipPanel;
     public Text TooltipText;
+    public float PaddingFromMouse;
 
-    private RectTransform _rect;
+    private RectTransform _panelRect;
     private bool _isHovering;
-    private Vector3 _offset;
 
 	void Start ()
     {
         Instance = this;
-        _rect = GetComponent<RectTransform>();
+        _panelRect = TooltipPanel.GetComponent<RectTransform>();
 	}
 	
 	void Update ()
     {
         TooltipPanel.SetActive(_isHovering);
 
-        //_offset = new Vector3(_rect.rect.width * 0.5f, -(_rect.rect.height * 0.5f), 0);
-        transform.position = Input.mousePosition/* + _offset*/;
+        if(_isHovering)
+        {
+            float width = _panelRect.rect.width;
+            float height = _panelRect.rect.height;
+
+            bool flipVertical = false;
+            bool flipHorizontal = false;
+            if(Input.mousePosition.x + width > Screen.width)
+            {
+                flipHorizontal = true;
+            }
+            if(Input.mousePosition.y - height < 0)
+            {
+                flipVertical = true;
+            }
+
+            Vector3 offset = new Vector2();
+            if(!flipHorizontal)
+            {
+                offset.x = PaddingFromMouse;
+            }
+            else
+            {
+                offset.x = -width + -PaddingFromMouse;
+            }
+            if (!flipVertical)
+            {
+                offset.y = -PaddingFromMouse;
+            }
+            else
+            {
+                offset.y = height + PaddingFromMouse;
+            }
+
+            transform.position = Input.mousePosition + offset;
+        }        
 	}
 
     public void AssignTooltip(string tooltipString)
